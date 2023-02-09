14442c45bcea578334a8228fdd0064b0d136dedc
hskuntze <hskuntze@gmail.com>
2023-02-09 12:52:55 -0300

    Project created

---
+++ src/test/java/br/com/kuntzeprojects/webscraping/WebScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kuntzeprojects.webscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
